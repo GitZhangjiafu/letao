7b4f7f82c6ffb54f4f90897a8336c203f5da64a8
gitzhangjiafu <931286855@qq.com>
2018-07-13 22:30:20 +0800

    index

---
+++ public/admin/js/common.js
@@ -0,0 +1,41 @@
+
+//开启进度条
+// NProgress.start();
+//关闭进度条
+// NProgress.done();
+
+//jquery全局事件
+//ajax请求开始时触发
+$(document).ajaxStart(function(){
+    NProgress.start();
+});
+//ajax请求结束时触发
+$(document).ajaxStop(function(){
+    setTimeout(function(){
+        NProgress.done();
+    },500)
+    
+});
+
+
+// 两级菜单隐藏事件
+$('.second').prev().on('click',function(){
+    $('.second').slideToggle();
+});
+
+$('.itcion').on('click',function(){
+    $('.lt_aside').toggleClass('active');
+    $('body').toggleClass('active');
+    $('.lt_head').toggleClass('active');
+});
+
+//模态框
+$('.tionst').on('click',function(){
+    $('#logant').modal('show');
+});
+
+
+
+
+
+
